6028b4db9882e5689ff454f32cb8c547e65df507
prabhat0101 <33635500+prabhat0101@users.noreply.github.com>
2019-09-15 15:02:17 +0530

    BinaryTree Implementation
    BinaryTree Implementation : Diameter and Height of BInary Tree.

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees.BinaryTree
+{
+    public static class BinaryTree
+    {
+
+        public static int LeftHeight = 0;
+        public static int RightHeight = 0;
+
+        public static Node CreateBinaryTree()
+        {
+            Node root = new Node(8);
+            root.Left = new Node(9);
+            root.Right = new Node(6);
+            root.Right.Left  = new Node(46);
+            root.Right.Right = new Node(64);
+            root.Left.Left = new Node(2);
+            root.Left.Right = new Node(7);
+            root.Left.Right.Right = new Node(1);
+            root.Left.Right.Left = new Node(16);
+            root.Left.Right.Right.Left = new Node(16);
+            root.Left.Right.Right.Left.Left = new Node(27);
+            root.Left.Right.Right.Left.Right = new Node(72);
+            return root;
+        }
+
+        public static int Height(Node root)
+        {
+            if (root == null)
+                return 0;
+            var leftHeight = Height(root.Left);
+            var rightHeight = Height(root.Right);
+            var height = 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);            
+            LeftHeight = leftHeight;
+            RightHeight = rightHeight;            
+            return height;
+        }       
+        
+    }
+
+    public class Node
+    {
+        public int Data = 0;
+        public Node Left = null;
+        public Node Right = null;
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }    
+
+    public class TreeStats
+    {
+        public int Height = 0;
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trees.BinaryTree;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var root = BinaryTree.CreateBinaryTree();            
+            //var treeStats = new TreeStats();
+            var height = BinaryTree.Height(root);
+            Console.WriteLine(height);
+            Console.WriteLine(BinaryTree.LeftHeight + BinaryTree.RightHeight + 1);
+            Console.ReadKey();
+        }
+    }
+}
