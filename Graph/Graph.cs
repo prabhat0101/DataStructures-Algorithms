1df6a76611580b0bf8e5f1da17041f9acfe812db
prabhat0101 <33635500+prabhat0101@users.noreply.github.com>
2019-09-19 08:01:54 +0530

    Added Graph Data Structure
    Added Graph Data Structure with DFS traversal and finding disconnected components count.

---
+++ Graph/Graph.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Graph
+    {
+        int VertexCount;
+        Dictionary<int, List<int>> Edges;
+
+        public Graph(int count)
+        {
+            VertexCount = count;
+            Edges = new Dictionary<int, List<int>>();
+            visited = new bool[count+1];
+        }
+
+        public bool[] visited;
+
+        public void AddEdge(int e1, int e2)
+        {        
+            List<int> values;
+            Edges.TryGetValue(e1, out values);
+            if (values == null)
+            {
+                values = new List<int>();
+                Edges.Add(e1, values);
+            }      
+            values.Add(e2);
+            //for undirected graph
+
+            Edges.TryGetValue(e2, out values);
+            if (values == null)
+            {
+                values = new List<int>();
+                Edges.Add(e2, values);
+            }
+            values.Add(e1);
+        }
+
+        public void DFS(int v)
+        {
+            visited[v] = true;
+            if (!Edges.ContainsKey(v) || Edges[v].Count == 0)
+            {
+                //Console.WriteLine(v);
+                return;
+            }
+
+            foreach (var i in Edges[v])
+            {
+                if (!visited[i])
+                {
+                    visited[i] = true;
+                    DFS(i);
+                }                 
+            }
+            //Console.WriteLine(v);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,11 +12,31 @@ namespace DataStructures
         static void Main(string[] args)
         {
 
-            var root = BinaryTree.CreateBinaryTree();            
-            //var treeStats = new TreeStats();
-            var height = BinaryTree.Height(root);
-            Console.WriteLine(height);
-            Console.WriteLine(BinaryTree.LeftHeight + BinaryTree.RightHeight + 1);
+            //var root = BinaryTree.CreateBinaryTree();            
+            //var height = BinaryTree.Height(root);
+            //Console.WriteLine(height);
+            //Console.WriteLine(BinaryTree.LeftHeight + BinaryTree.RightHeight + 1);
+
+            var v = int.Parse(Console.ReadLine());
+            var g = new Graph(v);
+            //Adding edges statically, in actual problem this has to be read from console.
+            g.AddEdge(1, 2);
+            g.AddEdge(3, 2);
+            g.AddEdge(4, 3);
+            g.AddEdge(6, 7);            
+
+            var disconnectedComponentCount = 0;
+
+            for(int i = 1; i < v+1; i++)
+            {
+                if (!g.visited[i])
+                {
+                    disconnectedComponentCount++;
+                    g.DFS(i);
+                }
+            }
+
+            Console.WriteLine(disconnectedComponentCount);
             Console.ReadKey();
         }
     }
